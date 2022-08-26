60072868fd60fbe437fc34b7f788028aeeed36ca
ignacio.silva <ignaciosilvag3@gmail.com>
2022-08-25 21:12:35 -0300

    agregue libreria

--- app.js
+++ app.js
@@ -27,6 +27,8 @@ const shopItemsData=[{
     img: "/Imagenes/Shorts/ahort3.jpg"}
 ]
 
+const basket = []
+
 const generateShop = () =>  {
 
 return (shop.innerHTML = shopItemsData.map((x)=>{
@@ -36,13 +38,13 @@ return `
             <img width="218" src= ${img} alt="">
             <div class="info">
                 <h3>${name}</h3>
-                <p>${desc}</p>
+                <p>${desc}</p> 
                 <div class="price-quantity">
                     <h2>${price} $</h2>
                     <div class="buttons">
-                        <i class="fa-solid fa-plus"></i>
-                        <div class="quantity">0</div>
-                        <i class="fa-solid fa-minus"></i>
+                        <i id="probandoBtn"  onclick ="increment(${id})" class="fa-solid fa-plus"></i>
+                        <div id=${id} class="quantity">0</div>
+                        <i onclick ="decrement(${id})"  class="fa-solid fa-minus"></i>
 
                     </div>
                 </div>
@@ -53,3 +55,44 @@ return `
 ); };
 
  generateShop();
+
+ const increment = (id) =>{
+    let selectedItem = id;
+   let search = basket.find((x) => x.id === selectedItem.id);
+   if (search === undefined){ 
+    basket.push({
+        id: selectedItem.id,
+        item: 1
+    })}
+    else{
+        search.item += 1;
+    }
+    // console.log(basket)
+    update(selectedItem.id);
+ };
+ const decrement = (id) =>{let selectedItem = id;
+    let search = basket.find((x) => x.id === selectedItem.id);
+    if (search.item === 0) return; 
+     else{
+         search.item -= 1;
+     }
+    //  console.log(basket)
+     update(selectedItem.id);
+ };
+ const update = (id) =>{
+     let search = basket.find((x)=> x.id === id);
+    console.log(search.item)
+    document.getElementById(id).innerHTML = search.item
+ };
+
+const probandoBtn = document.getElementById("probandoBtn")
+
+probandoBtn.addEventListener("click",() => {
+    Toastify({
+  text: "Elemento agregado al carrito",
+  duration: 3000,
+  position:"left",
+  backgroundColor: "#056745"
+ 
+}).showToast();
+})
